66bdaad7ba037fa400310f84e56dd6255dec1b22
User <User@HP-DM4>
2021-09-07 16:49:49 -0300

    Ordenação

---
+++ src/controller/QuickSort.java
@@ -0,0 +1,63 @@
+package controller;
+
+//import java.util.Arrays;
+//import java.util.Random;
+
+public class QuickSort {
+
+	
+/*	public static int[] gerar(int n) {
+		int[]v = new int[n];
+		Random gerador = new Random();
+		for (int i = 0; i < n; i++)
+			v[i] = gerador.nextInt(n);
+		
+		System.out.println(v);
+		
+		return v;*/
+				
+					
+	
+		
+	public int[] quicksort(int[] v, int esq, int dir){
+		
+		if (esq < dir) {
+			int j = separar (v, esq, dir);
+			quicksort(v, esq, j -1);
+			quicksort(v, j +1, dir);
+			
+		}
+		return v;
+	}
+	
+	
+
+	public int separar(int[] v, int esq, int dir) {
+		int i = esq + 1;
+		int j = dir;
+		int pivo = v[esq];
+		
+		while (i <= j) {
+			if (v[i] <= pivo)i++;
+			else if (v[j] > pivo) j--;
+			else if (i <= j) {
+				trocar(v, i, j);
+				i++;
+				j--;
+			}
+			
+		}
+		trocar(v, esq, j);
+		
+		return j;
+	}
+
+	public static void trocar(int[] v, int i, int j) {
+		int aux = v[i];
+		v[i] = v[j];
+		v[j] = aux;
+		
+	}
+	
+}
+
---
+++ src/view/Principal.java
@@ -0,0 +1,36 @@
+  
+package view;
+
+import java.util.Arrays;
+
+import controller.MergeSort;
+import controller.QuickSort; 
+import controller.BubbleSort;
+
+public class Principal {
+
+	
+
+	public static void main(String[] args) {
+		
+		QuickSort qSort = new QuickSort();
+		MergeSort Msort = new MergeSort();
+		BubbleSort bSort = new BubbleSort();
+		int[]v = {15,18,29,1,0,4,13,18,25,15,43,0};
+		int[]w = new int[v.length];
+		v = qSort.quicksort (v, 0, v.length -1);
+		w = Msort.mergeSort(v, w, v.length-1, 0);
+		bSort.bolha(v);
+		System.out.println("Ordenação em QuickSort: "+Arrays.toString( v ));
+//		System.out.println("");
+		System.out.println("====================================================================");
+		System.out.println("Ordenação em MergeSort: "+Arrays.toString( w ));
+//		System.out.println("");
+		System.out.println("====================================================================");
+		System.out.println("Ordenação em BubbleSort: "+Arrays.toString( v ));
+//		System.out.println("");
+		System.out.println("====================================================================");
+	}
+	
+
+}
